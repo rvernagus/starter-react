206e240e9aaf3a0c233dea984369cfbdaa49cb90
Ray Vernagus <r.vernagus@gmail.com>
2017-05-03 02:30:37 +0000

    Add Header and Footer

--- src/web/js/main.jsx
+++ src/web/js/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
+import Header from './components/common/Header.jsx'
+import Footer from './components/common/Footer.jsx'
 
 render(
-  <h1>React Dev Env!</h1>,
+  <div>
+    <Header />
+    <h1>React Dev Env!</h1>
+    <Footer />
+  </div>,
   document.getElementById('app')
 )
