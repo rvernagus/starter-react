47a529f2078ab26996aecc3b91ed475788ab1f4b
Ray Vernagus <r.vernagus@gmail.com>
2017-05-08 01:32:50 +0000

    Refactor duplicate server code

--- src/server/devServer.js
+++ src/server/devServer.js
@@ -1,22 +1,14 @@
 import express from 'express'
 import path from 'path'
 import morgan from 'morgan'
-import { setupRoutes } from './common'
+import { setupUses, setupRoutes, listen } from './common'
 
-const port = 8080
 const app = express()
 
 app.set('views', path.resolve(__dirname, 'views'))
 
 app.use(morgan('combined'))
-app.use(express.static(path.resolve(__dirname, 'wwwroot')))
 
+setupUses(app)
 setupRoutes(app)
-
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err) // eslint-disable-line no-console
-  } else {
-    console.log(`Running DEV server on http://localhost: ${port}`) // eslint-disable-line no-console
-  }
-})
+listen(app)
--- src/server/prodServer.js
+++ src/server/prodServer.js
@@ -1,20 +1,12 @@
 import express from 'express'
 import path from 'path'
 import compression from 'compression'
-import { setupRoutes } from './common'
+import { setupUses, setupRoutes, listen } from './common'
 
-const port = 8080
 const app = express()
 
 app.use(compression())
-app.use(express.static(path.resolve(__dirname, 'wwwroot')))
 
+setupUses(app)
 setupRoutes(app)
-
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err) // eslint-disable-line no-console
-  } else {
-    console.log(`Running PROD server on http://localhost: ${port}`) // eslint-disable-line no-console
-  }
-})
+listen(app)
